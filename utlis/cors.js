cd89780cf4a9164b6d55a6d5a6436e8801e6f0f0
itzrobeast <robizzell@gmail.com>
2024-11-25 00:49:10 -0800

    Delete utlis directory

--- utlis/cors.js
+++
@@ -1,7 +0,0 @@
-
-export const applyCors = (res) => {
-  // Add CORS headers
-  res.setHeader('Access-Control-Allow-Origin', 'https://mila-verse.vercel.app'); // Replace with your frontend domain
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Allowed methods
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allowed headers
-};
