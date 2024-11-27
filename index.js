adbce566c6468e832537bd29af4d02ed9068d8b6
itzrobeast <robizzell@gmail.com>
2024-11-26 17:52:11 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ const app = express();
 
 // Use JSON parsing middleware
 app.use(express.json());
-app.use(cors());
 
 // Use the cors package
-const allowedOrigin = process.env.ALLOWED_ORIGIN || 'https://mila-verse.vercel.app';
+const allowedOrigin = 'https://mila-verse.vercel.app';
 app.use(cors({
   origin: allowedOrigin,
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type'],
-  credentials: true, // Adjust if cookies are needed
+  credentials: true,
 }));
 
 // Mount the Instagram webhook router
@@ -23,17 +22,6 @@ app.use('/instagram-webhook', instagramWebhook);
 // Root route
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to the Node.js Serverless Function!');
-});
-
-// Fallback for unknown routes
-app.use((req, res) => {
-  res.status(404).send('Route not found');
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Internal Server Error');
 });
 
 export default app;
