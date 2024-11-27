915ce65f76491e2c396025750c72e1ade03709e6
itzrobeast <robizzell@gmail.com>
2024-11-26 19:27:14 -0800

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.use((req, res, next) => {
   console.log(`[DEBUG] Request to ${req.url} - Method: ${req.method}`);
   next();
 });
+app.use((req, res, next) => {
+  console.log(`[DEBUG] Middleware executed: ${req.method} ${req.url}`);
+  next();
+});
+
+
 
 // CORS configuration
 const allowedOrigin = 'https://mila-verse.vercel.app'; // Frontend URL
