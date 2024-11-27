7c193511defd924619cb5b2d26a0e200c6d13705
itzrobeast <robizzell@gmail.com>
2024-11-26 17:23:26 -0800

    Delete utils directory

--- utils/cors.js
+++
@@ -1,7 +0,0 @@
-
-export const applyCors = (res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://mila-verse.vercel.app'); // Allow only your frontend domain
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Allow methods
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow specific headers
-  res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow cookies if needed
-};
