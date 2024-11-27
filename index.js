1d5c002562446beb7f0723db7991299bc1bf115f
itzrobeast <robizzell@gmail.com>
2024-11-26 19:28:03 -0800

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,6 @@ app.use((req, res, next) => {
   console.log(`[DEBUG] Request to ${req.url} - Method: ${req.method}`);
   next();
 });
-app.use((req, res, next) => {
-  console.log(`[DEBUG] Middleware executed: ${req.method} ${req.url}`);
-  next();
-});
-
 
 
 // CORS configuration
