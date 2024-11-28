a4f2de5db7a2060a3e146f7631eab93f84b5d84f
itzrobeast <robizzell@gmail.com>
2024-11-28 12:37:09 -0800

    Update setup-business.js

--- setup-business.js
+++ setup-business.js
@@ -2,9 +2,10 @@ import express from 'express';
 
 const router = express.Router();
 
+// Simple POST endpoint
 router.post('/', (req, res) => {
-  console.log('[DEBUG] Request Body:', req.body);
-  res.status(200).json({ message: 'Test Successful' });
+  console.log('[DEBUG] /setup-business Body:', req.body);
+  res.status(200).json({ message: 'Success', body: req.body });
 });
 
 export default router;
