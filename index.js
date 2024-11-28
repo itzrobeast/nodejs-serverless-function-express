035196f4d26893fb60db7ee3022f17a47bc0c908
itzrobeast <robizzell@gmail.com>
2024-11-28 12:41:18 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 import express from 'express';
-import setupBusinessRouter from './setup-business.js';
 
 const app = express();
 
-app.use(express.json());
-
-// Route registration
-app.use('/setup-business', setupBusinessRouter);
-
-// Root health check
 app.get('/', (req, res) => {
-  res.status(200).send('Root Route is Working!');
+  res.status(200).send('Root is working!');
 });
 
 export default app;
