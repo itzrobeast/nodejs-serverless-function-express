0228693ea02ed08d5a67fb3d11b0bd256ac39cb9
itzrobeast <robizzell@gmail.com>
2024-12-06 22:17:02 -0800

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
-);
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
-export default supabase; // Ensure default export is present
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
