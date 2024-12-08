1801e5c5cb0014edc3d242544084919ed27d98bd
itzrobeast <robizzell@gmail.com>
2024-12-08 15:01:12 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
+});
+
+// Debugging route to list files
+app.get('/debug/files', (req, res) => {
+  const currentDir = path.resolve();
+  fs.readdir(currentDir, (err, files) => {
+    if (err) {
+      res.status(500).send({ error: err.message });
+    } else {
+      res.send({ files });
+    }
+  });
 });
 
 export default app;
