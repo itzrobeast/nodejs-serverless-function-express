0f478d1914fa9ba3bfd3115eb929ec31a8663926
itzrobeast <robizzell@gmail.com>
2024-12-08 18:15:58 -0800

    Update login.js

--- auth/login.js
+++ auth/login.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import supabase from './supabaseClient.js';
+import supabase from '../supabaseClient.js';
 import fetch from 'node-fetch';
 
 const router = express.Router();
