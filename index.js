acc5c9b677c4e2f7f129631fc631b3e421b174d2
itzrobeast <robizzell@gmail.com>
2025-01-27 01:09:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -178,9 +178,5 @@ process.on('SIGINT', async () => {
   process.exit(0);
 });
 
-process.on('SIGTERM', async () => {
-  console.log('[INFO] SIGTERM signal received: closing server...');
-  process.exit(0);
-});
 
 export default app;
